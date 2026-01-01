daff9244b7a16928148e6c042924f565d440daed
Gurneet singh <gurneetsinghjatt@gmail.com>
2026-01-01 09:03:57 +0530

    Change server port from 3000 to 8080

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ async fn main() {
         .with_state(db);
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     println!("server on http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
